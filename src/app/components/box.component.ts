0a2611f2ddd71eb8c1000dac57e147fd2fd42c77
joelstevick <joelstevick@outlook.com>
2021-03-02 16:13:36 -0500

    1 file modified

--- src/app/components/box.component.ts
+++ src/app/components/box.component.ts
@@ -3,6 +3,15 @@ import { Component } from "@angular/core";
 @Component({
   selector: "box",
   template: "<div>red</div>",
-  styles: []
+  styles: [
+    `div 
+    { 
+      height: 100px; 
+      line-height: 100px;
+      width: 100px;
+      text-align: center; 
+      border: 1px dashed grey;
+      `
+  ]
 })
 export class Box {}
